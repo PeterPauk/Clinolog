716b96c4d83183acad1fe850d0fa8f97ca25a189
PeterPauk <petkopauk@gmail.com>
2025-04-09 22:56:17 +0200

    info section

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/0a4f5afbb3.js" crossorigin="anonymous"></script>
 </head>
 
 
@@ -29,21 +30,25 @@
 
         <div class="info-section">
             <div>
-                <h3>Organize by System</h3>
+                <h3>Organized System</h3>
                 <p>Easily categorize diseases based on body systems (e.g., nervous, respiratory, digestive) to streamline your workflow and focus on relevant cases.</p>
+                <img src="./img/image1x1.png" alt="">
             </div>
             <div>
                 <h3>Track Patient Cases</h3>
                 <p>Log the number of patients with specific conditions, monitor their progress, and analyze trends to support better decision-making.</p>
+                <img src="./img/image2x1.png" alt="">
             </div>
             <div>
                 <h3>Customizable Notes</h3>
                 <p>Attach your own notes, observations, or medical insights to each disease entry, ensuring all relevant information is easily accessible for future reference.</p>
+                <img src="./img/image3x1.png" alt="">
             </div>
         </div>
     </main>
 </body>
 
+<?php   ?>
 
 
 
