c49ffc99670a043f919888840517b245f71d6342
PeterPauk <petkopauk@gmail.com>
2025-04-09 18:02:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clinolog</title>
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap" rel="stylesheet">
+</head>
+
+
+<body>
+    <header>
+        <nav>
+            <h2>Clinolog</h2>
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div class="landing-photo">
+            <h1>Improving healthcare. <span>Together.</span></h1>
+        </div>
+
+        <div class="info-section">
+            <div>
+                <h3>Organize by System</h3>
+                <p>Easily categorize diseases based on body systems (e.g., nervous, respiratory, digestive) to streamline your workflow and focus on relevant cases.</p>
+            </div>
+            <div>
+                <h3>Track Patient Cases</h3>
+                <p>Log the number of patients with specific conditions, monitor their progress, and analyze trends to support better decision-making.</p>
+            </div>
+            <div>
+                <h3>Customizable Notes</h3>
+                <p>Attach your own notes, observations, or medical insights to each disease entry, ensuring all relevant information is easily accessible for future reference.</p>
+            </div>
+        </div>
+    </main>
+</body>
+
+
+
+
+</html>
