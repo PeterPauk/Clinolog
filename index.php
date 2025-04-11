e5548c873a277d194a7e17ac429a0a05a463190d
PeterPauk <petkopauk@gmail.com>
2025-04-11 11:30:41 +0200

    translated landing page into slovak

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,37 +11,36 @@
     <script src="https://kit.fontawesome.com/0a4f5afbb3.js" crossorigin="anonymous"></script>
 </head>
 
-
 <body>
     <header>
         <nav>
             <h2>Clinolog</h2>
             <ul>
-                <li><a href="./pages/healthcard.php">Login</a></li>
-                <li><a href="#">Register</a></li>
+                <li><a href="./pages/healthcard.php">Prihlásiť</a></li>
+                <li><a href="#">Zaregistrovať</a></li>
             </ul>
         </nav>
     </header>
 
     <main>
         <div class="landing-photo">
-            <h1>Improving healthcare. <span>Together.</span></h1>
+            <h1>Zlepšujeme zdravotnú starostlivosť. <span>Spolu.</span></h1>
         </div>
 
         <section class="info-section">
             <div>
-                <h3>Organized System</h3>
-                <p>Easily categorize diseases based on body systems (e.g., nervous, respiratory, digestive) to streamline your workflow and focus on relevant cases.</p>
+                <h3>Organizovaný systém</h3>
+                <p>Jednoducho kategorizujte choroby podľa telesných systémov (napr. nervový, dýchací, tráviaci), aby ste zefektívnili svoju prácu a zamerali sa na relevantné prípady.</p>
                 <img src="./img/image1x1.png" alt="">
             </div>
             <div>
-                <h3>Track Patient Cases</h3>
-                <p>Log the number of patients with specific conditions, monitor their progress, and analyze trends to support better decision-making.</p>
+                <h3>Sledovanie prípadov pacientov</h3>
+                <p>Zaznamenávajte počet pacientov s konkrétnymi ochoreniami, sledujte ich pokrok a analyzujte trendy na podporu lepšieho rozhodovania.</p>
                 <img src="./img/image2x1.png" alt="">
             </div>
             <div>
-                <h3>Customizable Notes</h3>
-                <p>Attach your own notes, observations, or medical insights to each disease entry, ensuring all relevant information is easily accessible for future reference.</p>
+                <h3>Prispôsobiteľné poznámky</h3>
+                <p>Pridajte si vlastné poznámky, pozorovania alebo lekárske postrehy ku každej chorobe, aby ste mali všetky relevantné informácie ľahko dostupné aj v budúcnosti.</p>
                 <img src="./img/image3x1.png" alt="">
             </div>
         </section>
@@ -50,27 +49,25 @@
             <div class="main-div">
                 <img src="./img/doctor.jpg" alt="">
                 <div>
-                    <h3>Start making the world a better place</h3>
-                    <a href="#">Begin</a>
+                    <h3>Začnite robiť svet lepším miestom</h3>
+                    <a href="#">Začať</a>
                 </div>
             </div>
         </section>
 
         <footer>
             <div class="footer-links">
-                <a href="#">About Us</a>
-                <a href="#">Contact</a>
-                <a href="#">Privacy Policy</a>
-                <a href="#">Terms of Service</a>
+                <a href="#">O nás</a>
+                <a href="#">Kontakt</a>
+                <a href="#">Zásady ochrany osobných údajov</a>
+                <a href="#">Podmienky používania</a>
             </div>
 
-            <p>2025 Clinolog. All rights reserved.</p>
-    </footer>
+            <p>2025 Clinolog. Všetky práva vyhradené.</p>
+        </footer>
     </main>
 </body>
 
 <?php   ?>
-
-
 
 </html>
